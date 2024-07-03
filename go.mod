6d9ed35a2d05f13f95af7808ba267aa3848829d0
Yukti Aristotle <yukti.aristotle@gmail.com>
2024-07-02 21:23:05 -0700

    Cohere Hello World.

---
+++ go.mod
@@ -0,0 +1,10 @@
+module cohere
+
+go 1.22.1
+
+require github.com/cohere-ai/cohere-go/v2 v2.8.2
+
+require (
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+
+	cohere "github.com/cohere-ai/cohere-go/v2"
+	cohereclient "github.com/cohere-ai/cohere-go/v2/client"
+)
+
+var (
+	//go:embed chtoken
+	cohereAPIKey string
+)
+
+func main() {
+	client := cohereclient.NewClient(cohereclient.WithToken(cohereAPIKey))
+	response, err := client.Chat(
+		context.TODO(),
+		&cohere.ChatRequest{
+			Message: "Hello World!",
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	println(response.String())
+}
